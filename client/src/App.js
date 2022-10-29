56eb16c50764d3ea99d2158774ba1180b4b4c99b
Warrior of Zarona <andrewbautista85@gmail.com>
2022-10-28 23:11:16 -0400

    chore: added info to object and formatted date with dayjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import { Home } from './pages/Home/Home';
+import React from "react";
+import { Home } from "./pages/Home/Home";
 
 function App() {
-  return (
-    <Home />
-  );
+  return <Home />;
 }
 
 export default App;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,30 @@
-import './style.css';
+import React from "react";
+import "./style.css";
+import dayjs from "dayjs";
 
-export const Home = () => (
-    <div className='main'>
-        <img className='banner' src='./assets/img/banner.png' />
-        <h1></h1>
+export const Home = () => {
+  const event = {
+    name: "Potluck at Andrew (WoZ) and Michelle's Home",
+    address: "2466 Wilson Terrace",
+    city: "Union",
+    state: "NJ",
+    zipcode: "07083",
+    date: "2022-11-25",
+    phone: "908-922-6538",
+  };
+
+  const formattedDate = dayjs(event.date).format("MM/DD/YYYY");
+
+  return (
+    <div className="main">
+      <img className="banner" src="./assets/img/banner.png" />
+      <div className="info">
+        <h1>{event.name}</h1>
+        <h2>{event.address}</h2>
+        <h2>{`${event.city}, ${event.state} ${event.zipcode}`}</h2>
+        <h2>Date: {formattedDate}</h2>
+        <h2>Phone: {event.phone}</h2>
+      </div>
     </div>
-
-)
+  );
+};
