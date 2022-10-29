7c756bf04eedc9b83b58598776669c92a482fa4e
Warrior of Zarona <andrewbautista85@gmail.com>
2022-10-28 22:22:42 -0400

    feat: created Home component with image and basic styling, added to App.js

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+export const Home = () => (
+    <div className='main'>
+        <img className='banner' src='./assets/img/banner.png' />
+        <h1></h1>
+    </div>
+
+)
