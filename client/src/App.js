1b24aeaa88942fe2eb7be04f6de732a9c8ef04f6
Warrior of Zarona <andrewbautista85@gmail.com>
2022-10-30 20:30:36 -0400

    chore: added new components into App.js, adjusted index.css styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import MainContainer from "./components/MainContainer";
+import Header from "./components/Header";
 import Home from "./pages/Home";
+import dayjs from "dayjs";
+import CountdownTimer from "./components/CountdownTimer";
 
-function App() {
-  return <Home />;
-}
+import eventData from "./utils/eventData.json";
+
+const App = () => {
+  const { event, people } = eventData;
+  const eventDate = dayjs(event.date);
+  const daysToDate = eventDate.diff(dayjs());
+  const now = new Date().getTime();
+  const timeToDate = daysToDate + now;
+
+  return (
+    <MainContainer>
+      <Header eventData={event} />
+      {/* TODO: Home component should go to / route */}
+      <Home eventData={eventData} />
+      {/* TODO: Add route for Invite page component that goes to invite/:id */}
+
+    </MainContainer>
+  );
+};
 
 export default App;
