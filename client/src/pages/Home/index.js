6818624c5d2260ee9deacd9f45599c3e315e9578
Warrior of Zarona <andrewbautista85@gmail.com>
2022-10-30 20:30:14 -0400

    chore: removed code from Home component that was being used in new components

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,38 +1,17 @@
 import React from "react";
-import { Header, Subheader } from "../../components/Text";
+import { Subtitle } from "../../components/Text";
 import dayjs from "dayjs";
 import "./style.css";
 import { Person } from "../../components/Person";
 import eventData from "../../utils/eventData.json";
-import CountdownTimer from "../../components/CountdownTimer/CountdownTimer";
+import CountdownTimer from "../../components/CountdownTimer";
 
-const Home = () => {
-  const { event, people } = eventData;
-
-  const eventDate = dayjs(event.date);
-  const formattedDate = eventDate.format("MM/DD/YYYY h:mma");
-  const daysToDate = eventDate.diff(dayjs());
-  const now = new Date().getTime();
-  const timeToDate = daysToDate + now;
+const Home = ({eventData}) => {
+  const { people } = eventData;
   const sortAttendingPeople = people.sort((a) => (a.isAttending ? -1 : 1));
 
   return (
-    <div className="main">
-      <img
-        className="banner"
-        src="./assets/img/banner.png"
-        alt="Friendsgiving"
-      />
-      <div className="info">
-        <Header style={{ textAlign: "center" }}>{event.name}</Header>
-        <Subheader>{event.address}</Subheader>
-        <Subheader>
-          {event.city}, {event.state} {event.zipcode}
-        </Subheader>
-        <Subheader>Date: {formattedDate}</Subheader>
-        <Subheader>Phone: {event.phone}</Subheader>
-        <Subheader>Serving: {event.food}</Subheader>
-      </div>
+    <>
       <div className="hosts">
         {people.map(({ id, name, image, isHost }) =>
           isHost ? (
@@ -58,11 +37,10 @@ const Home = () => {
             ) : null
         )}
       </div>
-      <CountdownTimer targetDate={timeToDate} />
-      <Subheader>
+      <Subtitle>
         Note: If plans change, use the page sent to you to update your status!
-      </Subheader>
-    </div>
+      </Subtitle>
+    </>
   );
 };
 
