736e8759aea87b47c4821ddb22b7de00e0f5af28
Warrior of Zarona <andrewbautista85@gmail.com>
2022-11-05 21:55:34 -0400

    chore: updsted index.js in model

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,4 @@
 // Import models here and then module.export
-const Person = require('./Person');
-const Event = require('./Event');
+const Person = require("./Person");
 
-module.exports = { Person, Event};
+module.exports = { Person };
