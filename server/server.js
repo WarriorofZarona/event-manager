c7fe032cec059db458d3fb96df600387ead576b1
Warrior of Zarona <andrewbautista85@gmail.com>
2022-11-14 17:33:44 -0500

    chore: changed production server code

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ app.use(routes);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build", "index.html"), err => {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
 }
 
 app.get("/", (req, res) => {
